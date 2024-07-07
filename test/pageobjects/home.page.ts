cb6bea97bcc468b1587e27e822f35f50258a0d8c
Yoshihiro Tanaka <contact@cordea.jp>
2024-07-07 12:48:05 +0900

    check the element values

--- test/pageobjects/home.page.ts
+++ test/pageobjects/home.page.ts
@@ -1,28 +1,68 @@
 import { driver } from "@wdio/globals";
 
 class HomePage {
-  get example1() {
-    return driver.$("~example1");
+  async example1() {
+    const id = await driver.$("~example1");
+    const value = await driver.$("//*[@value='example1']");
+    const name = await driver.$("//*[@name='example1']");
+    const label = await driver.$("//*[@label='example1']");
+    expect(id.elementId).toBe(value.elementId);
+    expect(id.elementId).toBe(name.elementId);
+    expect(id.elementId).toBe(label.elementId);
+    return id;
   }
 
-  get example2() {
-    return driver.$("~example2-label\nexample2");
+  async example2() {
+    const id = await driver.$("~example2-label\nexample2");
+    const value = await driver.$("//*[@value='example2-label\nexample2']");
+    const name = await driver.$("//*[@name='example2-label\nexample2']");
+    const label = await driver.$("//*[@label='example2-label\nexample2']");
+    expect(id.elementId).toBe(value.elementId);
+    expect(id.elementId).toBe(name.elementId);
+    expect(id.elementId).toBe(label.elementId);
+    return id;
   }
 
-  get example3() {
-    return driver.$("~example3-id");
+  async example3() {
+    const id = await driver.$("~example3-id");
+    const value = await driver.$("//*[@value='example3']");
+    const name = await driver.$("//*[@name='example3-id']");
+    const label = await driver.$("//*[@label='example3']");
+    expect(id.elementId).toBe(value.elementId);
+    expect(id.elementId).toBe(name.elementId);
+    expect(id.elementId).toBe(label.elementId);
+    return id;
   }
 
-  get example4() {
-    return driver.$("~example4");
+  async example4() {
+    const id = await driver.$("~example4");
+    const value = await driver.$("//*[@value='example4-value']");
+    const name = await driver.$("//*[@name='example4']");
+    const label = await driver.$("//*[@label='example4']");
+    expect(id.elementId).toBe(value.elementId);
+    expect(id.elementId).toBe(name.elementId);
+    expect(id.elementId).toBe(label.elementId);
+    return id;
   }
 
-  get example5() {
-    return driver.$("~example5");
+  async example5() {
+    const id = await driver.$("~example5");
+    const name = await driver.$("//*[@name='example5']");
+    const label = await driver.$("//*[@label='example5']");
+    expect(id.elementId).toBe(name.elementId);
+    expect(id.elementId).toBe(label.elementId);
+    return id;
   }
 
-  get example6() {
-    return driver.$("~example6");
+  async example6() {
+    const id = await driver.$("~example6");
+    const value = await driver.$("//*[@value='example6']");
+    const name = await driver.$("//*[@name='example6']");
+    const label = await driver.$("//*[@label='example6']");
+    expect(id.elementId).toBe(value.elementId);
+    expect(id.elementId).toBe(name.elementId);
+    expect(id.elementId).toBe(label.elementId);
+    return id;
   }
 }
 
--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -2,12 +2,13 @@ import HomePage from "../pageobjects/home.page.ts";
 
 describe("My application", () => {
   it("should login with valid credentials", async () => {
-    await expect(HomePage.example1).toBeDisplayed();
-    await expect(HomePage.example2).toBeDisplayed();
-    await expect(HomePage.example3).toBeDisplayed();
-    await expect(HomePage.example4).toBeDisplayed();
-    await expect(HomePage.example5).toBeDisplayed();
-    await HomePage.example6.waitForDisplayed({ reverse: true });
-    await expect(HomePage.example6).toBeEnabled();
+    await expect(HomePage.example1()).toBeDisplayed();
+    await expect(HomePage.example2()).toBeDisplayed();
+    await expect(HomePage.example3()).toBeDisplayed();
+    await expect(HomePage.example4()).toBeDisplayed();
+    await expect(HomePage.example5()).toBeDisplayed();
+    await expect(HomePage.example5()).toBeDisabled();
+    await (await HomePage.example6()).waitForDisplayed({ reverse: true });
+    await expect(HomePage.example6()).toBeEnabled();
   });
 });
